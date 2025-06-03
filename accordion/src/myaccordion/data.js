2dc1fca781b3466b0b8a084fd531fac7f52ae172
Anushkaa-80 <161606518+Anushkaa-80@users.noreply.github.com>
2025-06-03 20:11:00 +0000

    Project 1

---
+++ accordion/src/myaccordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordion components?",
+        answer: "Accordion components are UI elements that allow users to expand and collapse sections of content. They help in organizing information in a limited space."
+    },
+    {
+        id: '2',
+        question: "What are they used for?",
+        answer: "They are commonly employed in various contexts like FAQs, menus, or content-heavy sections to enhance user experience by reducing clutter and improving navigation."
+    },
+    {
+        id: '3',
+        question: "Accordion as a musical instrument",
+        answer: "The accordion is a musical instrument that produces sound by pushing air through reeds using a bellows. It's commonly used in folk, classical, and contemporary music."
+    },
+    {
+        id: '4',
+        question: "How does an accordion component improve UX?",
+        answer: "Accordion components improve user experience by allowing users to easily access or hide specific content, reducing visual noise and helping users focus only on what they need."
+    }
+];
+
+export default data;
